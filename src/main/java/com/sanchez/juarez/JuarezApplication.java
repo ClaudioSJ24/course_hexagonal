301edbbc3a6815687a8f7c0c3907c256f5c0433e
Claudio Sanchez Juarez <sanjuacla@gmail.com>
2026-06-25 18:45:06 -0600

    add dependencies

---
+++ src/main/java/com/sanchez/juarez/JuarezApplication.java
@@ -0,0 +1,13 @@
+package com.sanchez.juarez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuarezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuarezApplication.class, args);
+	}
+
+}
